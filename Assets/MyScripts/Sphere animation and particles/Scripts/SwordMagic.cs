c5d35d56ff18d56e37de6decae9bd97b90eb5f6d
Galahad91 <alex.elia.91@gmail.com>
2018-01-21 23:47:54 +0100

    updated sword particles

--- Assets/MyScripts/Sphere animation and particles/Scripts/SwordMagic.cs
+++ Assets/MyScripts/Sphere animation and particles/Scripts/SwordMagic.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class SwordMagic : MagicStance
 {
-    Transform orbit1;
-    Transform orbit2;
-    ParticleSystem sword1;
-    ParticleSystem sword2;
-    ParticleSystem sword3;
-    ParticleSystem sword4;
-    ParticleSystem sword5;
-    ParticleSystem sword6;
-
+    public List<ParticleSystem> particles;
 
     void Awake ()
     {
@@ -26,12 +18,6 @@ public class SwordMagic : MagicStance
             anim = GetComponent<Animator>();
             crosshairPos = CrossHairManager.instance.crosshairPos;
             state = GameManager.instance.player.GetComponent<PlayerState>();
-            orbit1 = transform.GetChild(0);
-            orbit2 = transform.GetChild(1);
-
-            sword1 = orbit1.GetChild(0).GetChild(0).GetComponent<ParticleSystem>();
-            sword2 = orbit1.GetChild(1).GetChild(0).GetComponent<ParticleSystem>();
-            sword3 = orbit1.GetChild(2).GetChild(0).GetComponent<ParticleSystem>();
 
             impact = transform.GetChild(2).GetComponent<ParticleSystem>();
 
@@ -42,15 +28,12 @@ public class SwordMagic : MagicStance
 
     public override void ChangeStartColor()
     {
-        ParticleSystem.MainModule psmain1 = sword1.main;
-        psmain1.startColor = color.Evaluate(cRange);
-        ChangeColorAllParticles(sword1);
-        ParticleSystem.MainModule psmain2 = sword2.main;
-        psmain2.startColor = color.Evaluate(cRange);
-        ChangeColorAllParticles(sword2);
-        ParticleSystem.MainModule psmain3 = sword3.main;
-        psmain3.startColor = color.Evaluate(cRange);
-        ChangeColorAllParticles(sword3);
+        for (int i = 0; i < particles.Count; i++)
+        {
+            ParticleSystem.MainModule psmain1 = particles[i].main;
+            psmain1.startColor = color.Evaluate(cRange);
+            ChangeColorAllParticles(particles[i]);
+        }      
     }
 
     public override void UseSkill()
